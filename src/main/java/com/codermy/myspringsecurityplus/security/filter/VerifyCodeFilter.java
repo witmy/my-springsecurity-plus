150363fcaee5b808a0edf876bffd1cdaaa26498a
“userMY <2849823368@qq.com>
2020-10-11 09:28:59 +0800

    优化字符串大小写比较，增加session中验证码的非空判断

--- src/main/java/com/codermy/myspringsecurityplus/security/filter/VerifyCodeFilter.java
+++ src/main/java/com/codermy/myspringsecurityplus/security/filter/VerifyCodeFilter.java
@@ -1,9 +1,9 @@
 package com.codermy.myspringsecurityplus.security.filter;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.codermy.myspringsecurityplus.common.utils.Result;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -22,6 +22,7 @@ public class VerifyCodeFilter extends OncePerRequestFilter {
 
     private String defaultFilterProcessUrl = "/login";
     private String method = "POST";
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         if (method.equalsIgnoreCase(request.getMethod()) && defaultFilterProcessUrl.equals(request.getServletPath())) {
@@ -31,13 +32,17 @@ public class VerifyCodeFilter extends OncePerRequestFilter {
             //验证码的信息存放在seesion种，具体看EasyCaptcha官方解释
             String genCaptcha = (String) request.getSession().getAttribute("captcha");
             response.setContentType("application/json;charset=UTF-8");
-            if (StringUtils.isEmpty(requestCaptcha)){
+            if (StrUtil.isEmpty(requestCaptcha)){
                 //删除缓存里的验证码信息
                 session.removeAttribute("captcha");
                 response.getWriter().write(JSON.toJSONString(Result.error().message("验证码不能为空!")));
                 return;
             }
-            if (!genCaptcha.toLowerCase().equals(requestCaptcha.toLowerCase())) {
+            if (StrUtil.isEmpty(genCaptcha)){
+                response.getWriter().write(JSON.toJSONString(Result.error().message("验证码已失效!")));
+                return;
+            }
+            if (!StrUtil.equalsIgnoreCase(genCaptcha,requestCaptcha)){
                 session.removeAttribute("captcha");
                 response.getWriter().write(JSON.toJSONString(Result.error().message("验证码错误!")));
                 return;
